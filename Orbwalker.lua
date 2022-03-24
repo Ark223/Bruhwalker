b2f865d9fb160b2547989829f933ea4ba79e1beb
Ark223 <Ark223Neww@gmail.com>
2022-03-24 13:47:19 +0100

    1.19 - Add attack timer to API

--- Orbwalker.lua
+++ Orbwalker.lua
@@ -1947,7 +1947,7 @@ _G.orbwalker = {
     enable_move = function(self) orb.movementEnabled = true end,
     force_target = function(self, target) orb.forcedTarget = target end,
     get_animation_time = function(self) return orb:GetAnimationTime() end,
-    get_attack_timer = function(self) return orb.attackTimer end,
+    get_auto_attack_timer = function(self) return orb.attackTimer end,
     get_auto_attack_range = function(self, unit) return orb.data:GetAutoAttackRange(unit) end,
     get_orbwalker_target = function(self) return orb.lastTarget end,
     get_projectile_speed = function(self) return orb.data:GetProjectileSpeed() end,
